f31e7b105c6906a73fb2601f5f7de6a678e5229d
GodzillaJim <jimnanjoroge76@gmail.com>
2020-12-19 10:23:54 +0300

    Implemented React Routers, next Product details

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import {Navbar, Nav, Container} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant ='dark' collapseOneSelect expand="lg">
   <Container>
-  <Navbar.Brand href="/">SpurSoft</Navbar.Brand>
+  <LinkContainer to='/'>
+    <Navbar.Brand>SpurSoft</Navbar.Brand>
+  </LinkContainer>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
-      <Nav.Link href="/cart">
+      <LinkContainer to='/cart'>
+      <Nav.Link>
           <i className='fas fa-shopping-cart'></i>
           CART</Nav.Link>
-      <Nav.Link href="/login">
+      </LinkContainer>
+      <LinkContainer to='/login'>
+      <Nav.Link>
       <i className='fas fa-user'></i>
           SIGN</Nav.Link>
+      </LinkContainer>
     </Nav>
   </Navbar.Collapse>
   </Container>
