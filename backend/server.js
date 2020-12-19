340be185b1e97ab2380c2d00c7bf909b44ff7530
GodzillaJim <jimnanjoroge76@gmail.com>
2020-12-19 15:11:00 +0300

    Covered ES modules, next MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const app = express()
 const products = require('./data/products')
 
-
+dotenv.config()
 app.get('/api/products', (req,res) => {
     res.json(products)
 })
@@ -11,5 +12,5 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find((p)=>(p._id === req.params.id))
     res.json(product)
 })
-
-app.listen(5000, ()=>(console.log('Listening on 5000')))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>(console.log(`Listening in ${process.env.NODE_ENV} mode on port ${PORT}`)))
