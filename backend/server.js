579832f3616a20d76b15636d195fbf782de70097
GodzillaJim <jimnanjoroge76@gmail.com>
2020-12-20 12:23:50 +0300

    Added routes, fetching from DB

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 connectDB()
 const app = express()
-app.get('/api/products', (req,res) => {
-    res.json(products)
+
+app.get('/', (req,res) => {
+    res.send('API is running')
 })
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p)=>(p._id === req.params.id))
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>(console.log(`Listening in ${process.env.NODE_ENV} mode on port ${PORT}`)))
