5d2acbb62c6daf92162f032dda8e2fec52f0e2a9
GodzillaJim <jimnanjoroge76@gmail.com>
2020-12-20 20:36:43 +0300

    complete HomeScreen redux, loading component next

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
 import Product from '../components/Product'
+import { listProducts } from '../actions/productActions.js'
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector( state => state.productList)
+    const { loading, error, products } = productList
+
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    },[])
+        dispatch(listProducts())
+    },[dispatch])
+    
     return (
         <>
          <h1>Latest products</h1>
-         <Row>
+         {loading 
+         ? <h2>Loading...</h2> : error 
+         ? <h3>{error}</h3> 
+         : <Row>
             {
                 products.map((product) => (
                     <Col key = {product._id} sm={12} md={6} lg={4} xl={3}>
@@ -23,7 +27,8 @@ const HomeScreen = () => {
                     </Col>
                 ))
             }    
-         </Row>   
+         </Row>  }
+           
         </>
     )
 }
