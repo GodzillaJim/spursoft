745a6efd7a4a5595caf7cd9d863cab1d5cff87b8
GodzillaJim <jimnanjoroge76@gmail.com>
2020-12-22 12:25:20 +0300

    Complete backend authentication

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -23,6 +23,32 @@ const authUser = asyncHandler(async (req,res) => {
 
 })
 
+const registerUser = asyncHandler(async (req,res) => {
+    const { name, email, password } = req.body
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+    const user = await User.create({
+        name, email, password
+    })
+    if (user) {
+        res.status(201)
+        const result = {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        }
+        res.json(result)
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
 const getUserProfile = asyncHandler(async (req,res) => {
     const user = await User.findById(req.user._id)
     if (user) {
@@ -39,5 +65,5 @@ const getUserProfile = asyncHandler(async (req,res) => {
 })
 
 export {
-    authUser, getUserProfile
+    authUser, getUserProfile, registerUser
 }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,5 +27,12 @@ const userSchema = mongoose.Schema({
 userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password)
 }
+userSchema.pre('save', async function (next ) {
+    if (!this.isModified('password')) {
+        next()
+    }
+    const salt = await bcrypt.genSalt(12)
+    this.password = await bcrypt.hash(this.password, salt)
+})
 
 export default mongoose.model('User', userSchema)
