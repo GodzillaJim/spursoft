aa1df4c93afe180a44a73534e6e6ae140b0d87ae
GodzillaJim <jimnanjoroge76@gmail.com>
2021-01-05 01:03:43 +0300

    create product, update product backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,26 @@
-import express from 'express'
-import { getProducts, getProductsById, deleteProduct } from '../controllers/productControllers.js'
-import {} from '../middleware/authMiddleware.js'
-const router = express.Router()
+import express from 'express';
+import {
+  getProducts,
+  getProductsById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+} from '../controllers/productControllers.js';
+import {} from '../middleware/authMiddleware.js';
+const router = express.Router();
 
-// @desc Fetch all products 
+// @desc Fetch all products
 // @route GET /api/products
 // @access public
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
-// @desc Fetch one products 
+// @desc Fetch one products
 // @route GET /api/products:id
 // @access public
-router.route('/:id').get(getProductsById).delete(protect, admin, deleteProduct )
+router
+  .route('/:id')
+  .get(getProductsById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
-export default router
+export default router;
